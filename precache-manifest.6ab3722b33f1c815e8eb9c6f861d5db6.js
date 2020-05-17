203792f929ac5848e21124015d78a86d61761de7
David Milosevic <milosevicdavid@gmail.com>
2020-05-17 19:00:10 +0200

    Updates

---
+++ precache-manifest.6ab3722b33f1c815e8eb9c6f861d5db6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecc07ae7d098a9300af9aa73686c0e2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "567febf3177ae4e70d3d",
+    "url": "/monsters-rolodex/static/css/main.b3a8fd40.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567febf3177ae4e70d3d",
+    "url": "/monsters-rolodex/static/js/main.a52f0509.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
